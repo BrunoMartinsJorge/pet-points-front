fb90e3f76f838fe9f7e34188ad4cc3f43f82bd35
brunomartinsjorge <rexon300008@gmail.com>
2026-03-04 14:48:18 -0300

    feat: Implementar interceptors

---
+++ src/app/core/interceptors/token-api-interceptor.ts
@@ -0,0 +1,43 @@
+import type { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { TokenService } from '../services/token-service';
+import { Router } from '@angular/router';
+import { environment } from '../environments/environment-dev';
+
+export const tokenApiInterceptor: HttpInterceptorFn = (req, next) => {
+  // Adiciona o serviço de autenticação
+  const tokenService = inject(TokenService);
+
+  // Adiciona o serviço de roteamento do Angular
+  const router = inject(Router);
+
+  // Pega a token do usuário
+  const token = tokenService.getToken;
+
+  // Verifica se o token expirou
+  if (!token || tokenService.isTokenExpired(token))
+    router.navigate(['/autenticacao']);
+
+  // Pega o ambiente de desenvolvimento ou produção
+  const env = environment;
+
+  // Pega a URL da API do ambiente
+  const apiUrl = env.api;
+
+  // Verifica se a URL da requisição começa com a URL da API
+  if (!req.url.startsWith(apiUrl))
+    // Adiciona a URL da API ao início da requisição, caso ela não comece com a URL da API
+    req = req.clone({ url: `${apiUrl}${req.url}` });
+
+  // Adiciona o token de autenticação ao cabeçalho da requisição, caso ele exista
+  if (token != null) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+  }
+
+  // Passa a requisição para o próximo interceptor ou para o backend, caso não haja mais interceptors
+  return next(req);
+};
