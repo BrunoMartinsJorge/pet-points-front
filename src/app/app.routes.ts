c005c8de636bb2bdb27396e9bcfd1d5ddb95f986
brunomartinsjorge <rexon300008@gmail.com>
2026-03-04 14:50:44 -0300

    feat: Implementar algumas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
+import { ROTAS_CLIENTES } from './features/cliente/cliente.route';
+import { Autenticacao } from './features/autenticacao/autenticacao';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...ROTAS_CLIENTES,
+    {
+        path: 'autenticacao',
+        component: Autenticacao,
+        title: 'Autenticação de Usuário'
+    },
+    {
+        path: '',
+        redirectTo: '/autenticacao',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/autenticacao',
+        pathMatch: 'full'
+    },
+];
