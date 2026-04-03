2c903b4fba829d524d941381e7981579e18cbd48
brunomartinsjorge <rexon300008@gmail.com>
2026-04-02 22:38:58 -0300

    feat: Implementar toasts ao TS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TokenService } from './core/services/token-service';
+import { Toast } from "primeng/toast";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toast],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
