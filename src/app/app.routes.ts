048a22eab19aefba2cd42f9607f1297d93d74d32
brunomartinsjorge <rexon300008@gmail.com>
2026-04-02 22:48:47 -0300

    feat: Atualizar rotas, styles e bibliotécas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import type { Routes } from '@angular/router';
-import { ROTAS_CLIENTES } from './features/cliente/cliente.route';
-import { Autenticacao } from './features/autenticacao/autenticacao';
+import { ROTAS_CLIENTES } from './modules/cliente/cliente.route';
+import { Autenticacao } from './modules/autenticacao/autenticacao';
+import { ROTAS_GERENTE } from './modules/gerente/gerente.routes';
 
 export const routes: Routes = [
     ...ROTAS_CLIENTES,
+    ...ROTAS_GERENTE,
     {
         path: 'autenticacao',
         component: Autenticacao,
