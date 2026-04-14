742efdcfd9c5a1c68fe09ba7119a5f180dad5412
brunomartinsjorge <rexon300008@gmail.com>
2026-04-14 01:07:04 -0300

    feat: Implementar rotas de estoquistas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import type { Routes } from '@angular/router';
 import { ROTAS_CLIENTES } from './modules/cliente/cliente.route';
 import { Autenticacao } from './modules/autenticacao/autenticacao';
 import { ROTAS_GERENTE } from './modules/gerente/gerente.routes';
+import { ROTAS_ESTOQUISTAS } from './modules/estoquista/estoquista.routes';
 
 export const routes: Routes = [
     ...ROTAS_CLIENTES,
     ...ROTAS_GERENTE,
+    ...ROTAS_ESTOQUISTAS,
     {
         path: 'autenticacao',
         component: Autenticacao,
