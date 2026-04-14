82416f88ad340264b42b16c3e584714e5996ef68
brunomartinsjorge <rexon300008@gmail.com>
2026-04-14 01:12:02 -0300

    feat: Implementação basica de componente de cards de consultas

---
+++ src/app/modules/cliente/pages/minhas-consultas/components/card-consultas-cliente/card-consultas-cliente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-consultas-cliente',
+  imports: [],
+  templateUrl: './card-consultas-cliente.html',
+  styleUrl: './card-consultas-cliente.scss',
+})
+export class CardConsultasCliente {
+
+}
