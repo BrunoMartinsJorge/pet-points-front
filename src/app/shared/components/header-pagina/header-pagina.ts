9ec0753d56aff7b2452e502d669dcf429d7a685b
brunomartinsjorge <rexon300008@gmail.com>
2026-04-18 01:36:05 -0300

    feat: Implementar WS de notificações ao header

--- src/app/shared/components/header-pagina/header-pagina.ts
+++ src/app/shared/components/header-pagina/header-pagina.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NavigationEnd, Router } from '@angular/router';
 import { BadgeModule } from 'primeng/badge';
@@ -7,9 +8,15 @@ import { OverlayBadgeModule } from 'primeng/overlaybadge';
 import { filter } from 'rxjs';
 import { RotasService } from '../../../core/services/rotas-service';
 import { PrimeNGModule } from '../../modules/prime-ng/prime-ng-module';
-import { TokenService } from '../../../core/services/token-service';
 import { ThemeService } from '../../../core/services/theme-service';
 import type { RoutesModel } from '../../models/RoutesModel';
+import type { Popover } from 'primeng/popover';
+import { PopoverModule } from 'primeng/popover';
+import type { NotificacaoDto } from '../../services/ws/notificacoes-ws-service';
+import { NotificacoesWsService } from '../../services/ws/notificacoes-ws-service';
+import { NotificacoesService } from '../../services/notificacoes-service';
+import type { CheckboxChangeEvent } from 'primeng/checkbox';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -20,16 +27,31 @@ import type { RoutesModel } from '../../models/RoutesModel';
     PrimeNGModule,
     BadgeModule,
     OverlayBadgeModule,
+    PopoverModule,
+    CheckboxModule,
   ],
   templateUrl: './header-pagina.html',
   styleUrl: './header-pagina.scss',
 })
-export class HeaderPagina {
+export class HeaderPagina implements OnInit {
+  @ViewChild('op') op!: Popover;
+
   public checked = false;
   private rotasService = inject(RotasService);
-  private readonly tokenService = inject(TokenService);
+  private notificacoesWs = inject(NotificacoesWsService);
+  private readonly notificacoesService = inject(NotificacoesService);
+  public notificacoes: NotificacaoDto[] = [];
+  public foiAbertoNotificiacoes = false;
+  public notificacoesParaMarcarComoLidas: number[] = [];
+
   private readonly router = inject(Router);
   private readonly themeService = inject(ThemeService);
+
+  ngOnInit(): void {
+    this.notificacoesService.notificacoes$.subscribe((n) => {
+      this.notificacoes = n;
+    });
+  }
 
   constructor() {
     this.router.events
@@ -39,15 +61,23 @@ export class HeaderPagina {
           this.rotasService.setarUltimaRota(event.urlAfterRedirects || '');
         }
       });
-    // this.notif.notificacao$.subscribe(n => this.notificacoes = n);
+    this.notificacoesWs.connect();
   }
 
-  /*public get getNotificacoes(): Notificacao[] {
-    return this.notificacoes;
-  }*/
+  public alterarPopover(): void {
+    this.op.toggle(event);
+    this.foiAbertoNotificiacoes = true;
+  }
 
   public voltar(): void {
     window.history.back();
+  }
+
+  public notificacaoMarcadaComoLida(id: number): boolean {
+    const notificacao = this.notificacoesParaMarcarComoLidas.find(
+      (n) => n === id,
+    );
+    return notificacao ? true : false;
   }
 
   public get rota(): RoutesModel {
@@ -63,7 +93,7 @@ export class HeaderPagina {
     if (!isNaN(Number(rotaAtual))) {
       rotaAtual = rotasPartes[rotasPartes.length - 2];
     }
-    
+
     return rotas.find((rota) => rota.path.includes(rotaAtual));
   }
 
@@ -71,7 +101,32 @@ export class HeaderPagina {
     this.themeService.toggleTheme();
   }
 
-  public limparNotificacoes(): void {
-    // this.notif.limparNotificacoes();
+  public fehcarPopover(): void {
+    this.notificacoesParaMarcarComoLidas = [];
+    this.notificacoesParaMarcarComoLidas = [
+      ...this.notificacoesParaMarcarComoLidas,
+    ];
+    this.op.hide();
+  }
+
+  public marcarSelecionadasComoLidas(todas: boolean): void {
+    if (todas)
+      this.notificacoesParaMarcarComoLidas = this.notificacoes.map((n) => n.id);
+    if (this.notificacoesParaMarcarComoLidas.length == 0) return;
+    this.notificacoesService.marcarNotificacoesComoLidas(
+      this.notificacoesParaMarcarComoLidas,
+    );
+  }
+
+  public marcarComoLida(
+    event: CheckboxChangeEvent,
+    idNotificacao: number,
+  ): void {
+    const notificacao = this.notificacoes.find((n) => n.id === idNotificacao);
+    if (!notificacao) return;
+    if (event.checked) this.notificacoesParaMarcarComoLidas.push(idNotificacao);
+    else
+      this.notificacoesParaMarcarComoLidas =
+        this.notificacoesParaMarcarComoLidas.filter((n) => n !== idNotificacao);
   }
 }
