b41b2e1fb55bac4c5d6e4cb446043b3f3a6da1a2
brunomartinsjorge <rexon300008@gmail.com>
2026-05-17 01:04:10 -0300

    feat: Adicionar toast component, aparentemente alguns componentes não conseguem utiliza-lo ser ser assim. VER DPS O PQ

--- src/app/modules/cliente/cliente.ts
+++ src/app/modules/cliente/cliente.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderPagina } from "../../shared/components/header-pagina/header-pagina";
 import { Sidebar } from "../../shared/components/sidebar/sidebar";
+import { PrimeNGModule } from '../../shared/modules/prime-ng/prime-ng-module';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-cliente',
-  imports: [RouterOutlet, HeaderPagina, Sidebar],
+  imports: [RouterOutlet, HeaderPagina, Sidebar, PrimeNGModule],
+  providers: [MessageService],
   templateUrl: './cliente.html',
   styleUrl: './cliente.scss',
 })
