0a951103c38dfb3b85c04d2ca4a73064bb37aad4
brunomartinsjorge <rexon300008@gmail.com>
2026-05-31 15:22:03 -0300

    feat: Implementar rotas - ATRASADO

--- src/app/modules/cliente/cliente.route.ts
+++ src/app/modules/cliente/cliente.route.ts
@@ -6,6 +6,8 @@ import { MeusPets } from './pages/meus-pets/meus-pets';
 import { InformacoesPet } from './pages/meus-pets/pages/informacoes-pet/informacoes-pet';
 import { AdicionarNovoPet } from './pages/meus-pets/pages/adicionar-novo-pet/adicionar-novo-pet';
 import { MinhasConsultas } from './pages/minhas-consultas/minhas-consultas';
+import { MeusAtendimentos } from './pages/meus-atendimentos/meus-atendimentos';
+import { MeusPagamentos } from './pages/meus-pagamentos/meus-pagamentos';
 
 export const ROTAS_CLIENTES: Routes = [
   {
@@ -84,11 +86,39 @@ export const ROTAS_CLIENTES: Routes = [
           visible: true,
           nome: 'Minhas Consultas',
           icone: 'pi pi-calendar',
-          group: '',
+          group: 'CONSULTAS',
           descricao: 'Consultas dos meus Pets',
         },
         canActivate: [authGuard],
-      }
+      },
+      {
+        path: 'meus-atendimentos',
+        title: 'Meus Atendimentos',
+        component: MeusAtendimentos,
+        data: {
+          RULE: 'CLIENTE',
+          visible: true,
+          nome: 'Atendimentos',
+          icone: 'pi pi-comments',
+          group: '',
+          descricao: 'Atendimentos do cliente',
+        },
+        canActivate: [authGuard],
+      },
+      {
+        path: 'meus-pagamentos',
+        title: 'Meus Pagamento',
+        component: MeusPagamentos,
+        data: {
+          RULE: 'CLIENTE',
+          visible: true,
+          nome: 'Pagamentos',
+          icone: 'pi pi-credit-card',
+          group: 'CONSULTAS',
+          descricao: 'Histórico de Pagamentos',
+        },
+        canActivate: [authGuard],
+      },
     ],
   },
 ];
