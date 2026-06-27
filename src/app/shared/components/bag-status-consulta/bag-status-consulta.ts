41ec2f2b8cb995a57fc3c6d1afaaae5b11ef126f
brunomartinsjorge <rexon300008@gmail.com>
2026-06-26 22:07:01 -0300

    style: Refatorar layout de badges de status de consulta

--- src/app/shared/components/bag-status-consulta/bag-status-consulta.ts
+++ src/app/shared/components/bag-status-consulta/bag-status-consulta.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import type { StatusConsultaEnum } from '../../models/enums/StatusConsultaEnum';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bag-status-consulta',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bag-status-consulta.html',
   styleUrl: './bag-status-consulta.scss',
 })
@@ -11,6 +12,7 @@ export class BagStatusConsulta {
   @Input() status: StatusConsultaEnum | null | undefined = null;
 
   public get getTexto(): string {
-    return this.status === null || this.status === undefined ? '' : this.status.toString()[0] + this.status.toString().slice(1).toLowerCase();
+    if (this.status == null || this.status == undefined) return '';
+    return this.status.toString();
   }
 }
