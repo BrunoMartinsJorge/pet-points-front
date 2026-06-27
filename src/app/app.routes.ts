6d28eff8454b41c13c85d81f731221e7e5c48370
brunomartinsjorge <rexon300008@gmail.com>
2026-06-26 22:05:46 -0300

    feat: Incorporar rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,24 @@ import { ROTAS_CLIENTES } from './modules/cliente/cliente.route';
 import { Autenticacao } from './modules/autenticacao/autenticacao';
 import { ROTAS_GERENTE } from './modules/gerente/gerente.routes';
 import { ROTAS_ESTOQUISTAS } from './modules/estoquista/estoquista.routes';
+import { ROTAS_VETERINARIOS } from './modules/veterinario/veterinario.route';
+import { ROTAS_ATENDENETES } from './modules/atendente/atendente.routes';
 
 export const routes: Routes = [
     ...ROTAS_CLIENTES,
     ...ROTAS_GERENTE,
+    ...ROTAS_VETERINARIOS,
     ...ROTAS_ESTOQUISTAS,
+    ...ROTAS_ATENDENETES,
     {
         path: 'autenticacao',
         component: Autenticacao,
         title: 'Autenticação de Usuário'
+    },
+    {
+        path: 'perfil',
+        loadComponent: () => import('./shared/pages/perfil/perfil').then(m => m.Perfil),
+        title: 'Perfil do Usuário'
     },
     {
         path: '',
