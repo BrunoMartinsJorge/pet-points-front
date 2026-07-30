8fe2c8e9f3de3061df41764a4d4d969479e7d2f6
brunomartinsjorge <rexon300008@gmail.com>
2026-07-29 23:55:53 -0300

    feat: Implementar add cliente

--- src/app/shared/pages/clientes-clinica/clientes-clinica.ts
+++ src/app/shared/pages/clientes-clinica/clientes-clinica.ts
@@ -3,15 +3,30 @@ import { Component, inject } from '@angular/core';
 import { PrimeNGModule } from '../../modules/prime-ng/prime-ng-module';
 import type { ClienteDto } from './model/ClienteDto';
 import { ClientesClinicaService } from './service/clientes-clinica-service';
-import { GeneroEnumOpcoes } from '../../models/enums/GeneroEnum';
+import {
+  GeneroEnum,
+  GeneroEnumOpcoes,
+  GeneroEnumOpcoesFormulario,
+} from '../../models/enums/GeneroEnum';
 import { GeneroBag } from '../../components/genero-bag/genero-bag';
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { Router } from '@angular/router';
 import { TokenService } from '../../../core/services/token-service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { StepperModule } from 'primeng/stepper';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
   selector: 'app-clientes-clinica',
-  imports: [PrimeNGModule, GeneroBag, ToggleButtonModule],
+  imports: [
+    PrimeNGModule,
+    GeneroBag,
+    ToggleButtonModule,
+    StepperModule,
+    DatePickerModule,
+    InputMaskModule,
+  ],
   templateUrl: './clientes-clinica.html',
   styleUrl: './clientes-clinica.scss',
 })
@@ -23,7 +38,8 @@ export class ClientesClinica implements OnInit {
   private clientes: ClienteDto[] = [];
   public clientesFiltrados: ClienteDto[] = [];
 
-  public readonly generos = GeneroEnumOpcoes;
+  public readonly generosFiltros = GeneroEnumOpcoes;
+  public readonly generos = GeneroEnumOpcoesFormulario;
   public filtros = {
     nome: '',
     genero: '',
@@ -32,12 +48,16 @@ export class ClientesClinica implements OnInit {
 
   public tipoUsuario = '';
 
+  public novoClienteForm!: FormGroup;
+  public visibilidadeDialogNovoCliente = true;
+
   ngOnInit(): void {
     this.buscarClientes();
     const token = this.tokenService.getTokenPayload;
     if (!token) return;
     const permissao = token.permissoes[0] == 'A' ? 'ATENDENTE' : 'GERENTE';
     this.tipoUsuario = permissao;
+    this.novoClienteForm = this.gerarFormularioNovoCliente();
   }
 
   private buscarClientes(): void {
@@ -67,7 +87,10 @@ export class ClientesClinica implements OnInit {
   }
 
   public verDetalhesCliente(idCliente: number): void {
-    this.router.navigate([this.tipoUsuario.toLocaleLowerCase() + '/detalhes-clientes', idCliente]);
+    this.router.navigate([
+      this.tipoUsuario.toLocaleLowerCase() + '/detalhes-clientes',
+      idCliente,
+    ]);
   }
 
   public limparFiltros(): void {
@@ -82,7 +105,7 @@ export class ClientesClinica implements OnInit {
     this.carregandoRelatorio = true;
     const form = {
       nome: this.filtros.nome,
-      genero: this.filtros.genero
+      genero: this.filtros.genero,
     };
     this.service.gerarRelatorioClientes(form).subscribe({
       next: (res: Blob) => {
@@ -93,7 +116,65 @@ export class ClientesClinica implements OnInit {
       },
       error: () => {
         this.carregandoRelatorio = false;
-      }
+      },
     });
+  }
+
+  private gerarFormularioNovoCliente(): FormGroup {
+    return new FormGroup({
+      nome: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(255),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(2),
+        Validators.maxLength(255),
+      ]),
+      genero: new FormControl(GeneroEnum.MASCULINO, [Validators.required]),
+      telefone: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(11),
+      ]),
+      cpf: new FormControl('', [
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11),
+      ]),
+      dataNascimento: new FormControl('', [Validators.required]),
+      senha: new FormControl('123456', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(255),
+      ]),
+    });
+  }
+
+  public fecharDialogNovoCliente(): void {
+    this.visibilidadeDialogNovoCliente = false;
+    this.novoClienteForm.reset();
+  }
+
+  public get primeiraEtapaNovoClienteValida(): boolean {
+    if (!this.novoClienteForm) return false;
+    const nome = this.novoClienteForm.get('nome');
+    const email = this.novoClienteForm.get('email');
+    const senha = this.novoClienteForm.get('senha');
+    if (!nome || !email || !senha) return false;
+    return nome.value && email.value && senha.value;
+  }
+
+  public get segundaEtapaNovoClienteValida(): boolean {
+    if (!this.novoClienteForm) return false;
+    if (!this.primeiraEtapaNovoClienteValida) return false;
+    const genero = this.novoClienteForm.get('genero');
+    const telefone = this.novoClienteForm.get('telefone');
+    const cpf = this.novoClienteForm.get('cpf');
+    const dataNascimento = this.novoClienteForm.get('dataNascimento');
+    if (!genero || !telefone || !cpf || !dataNascimento) return false;
+    return genero.value && telefone.value && cpf.value && dataNascimento.value;
   }
 }
