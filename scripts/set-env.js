0f577f559418ee15e7a737f7be335df3865b5adc
brunomartinsjorge <rexon300008@gmail.com>
2026-08-09 16:48:29 -0300

    feat: Credenciais de produção

---
+++ scripts/set-env.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const envConfigFile = `export const environment = {
+  production: true,
+  urlBack: '${process.env['URL_BACK']}',
+};
+`;
+
+fs.writeFileSync('./src/environments/environment.prod.ts', envConfigFile);
+
+console.log('Arquivo environment.prod.ts gerado com sucesso!');
